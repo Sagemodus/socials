6070b2e759161bda1ed7dc3e4f891e8ad5c654f0
Sagemodus <dejan.pantos@gmail.com>
2023-07-18 17:19:09 +0200

    optimierung search swipebar

--- src/components/swipenavigationcomponentsuche.vue
+++ src/components/swipenavigationcomponentsuche.vue
@@ -1,110 +1,136 @@
 <template>
-    <div class="navigation">
-      <div class="tabs">
+  <div class="navigation">
+    <div class="tabs" ref="tabsContainer">
+      <div class="tabs-inner">
         <div v-for="tab in tabs" :key="tab.path" class="tab" @click="switchTab(tab.path)" :class="{ active: activeTab === tab.path }">
           {{ tab.name }}
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { ref, reactive } from 'vue';
-  import Hammer from 'hammerjs';
-  import { iconColor } from './farben'
-  
-  export default {
-    name: 'SwipeNavigation',
-    props: ['onTabSwitch'],
-    data() {
-      return {
-        iconColor
-      }
-    },
-    setup(props) {
-      const tabs = reactive([
-        { name: 'Popular', path: '/popular' },
-        { name: 'Recent', path: '/recent' },
-        { name: 'People', path: '/people' },
-        { name: 'Conversation', path: '/conversation' },
-      ]);
-  
-      const activeTab = ref(tabs[0].path);
-  
-      const tabIndexes = tabs.reduce((acc, tab, i) => ({ ...acc, [tab.path]: i }), {});
-  
-      const switchTab = (path) => {
-        activeTab.value = path;
-        props.onTabSwitch(path);
-      };
-  
-      const switchToTabByIndex = (index) => {
-        if (index < 0 || index >= tabs.length) return;
-        switchTab(tabs[index].path);
-      };
-  
-      const nextTab = () => {
-        const currentIndex = tabIndexes[activeTab.value];
-        switchToTabByIndex(currentIndex + 1);
-      };
-  
-      const previousTab = () => {
-        const currentIndex = tabIndexes[activeTab.value];
-        switchToTabByIndex(currentIndex - 1);
-      };
-  
-      return { activeTab, nextTab, previousTab, switchTab, tabs };
-    },
-    mounted() {
+  </div>
+</template>
+
+<script>
+import { ref, reactive, onMounted } from 'vue';
+import Hammer from 'hammerjs';
+import { iconColor } from './farben'
+
+export default {
+  name: 'SwipeNavigation',
+  props: ['onTabSwitch'],
+  data() {
+    return {
+      iconColor
+    }
+  },
+  setup(props) {
+    const tabs = reactive([
+      { name: 'Popular', path: '/popular' },
+      { name: 'Recent', path: '/recent' },
+      { name: 'People', path: '/people' },
+      { name: 'Conversation', path: '/conversation' },
+    ]);
+
+    const activeTab = ref(tabs[0].path);
+
+    const tabIndexes = tabs.reduce((acc, tab, i) => ({ ...acc, [tab.path]: i }), {});
+
+    const switchTab = (path) => {
+      activeTab.value = path;
+      props.onTabSwitch(path);
+    };
+
+    const switchToTabByIndex = (index) => {
+      if (index < 0 || index >= tabs.length) return;
+      switchTab(tabs[index].path);
+    };
+
+    const nextTab = () => {
+      const currentIndex = tabIndexes[activeTab.value];
+      switchToTabByIndex(currentIndex + 1);
+    };
+
+    const previousTab = () => {
+      const currentIndex = tabIndexes[activeTab.value];
+      switchToTabByIndex(currentIndex - 1);
+    };
+
+    onMounted(() => {
       const el = document;
       const hammer = new Hammer(el);
       hammer.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
-      hammer.on('swiperight', this.previousTab);
-      hammer.on('swipeleft', this.nextTab);
-      document.documentElement.style.setProperty('--iconColor', this.iconColor);
-    },
-  };
-  </script>
-  
-  <style scoped>
+      hammer.on('swiperight', previousTab);
+      hammer.on('swipeleft', nextTab);
+      document.documentElement.style.setProperty('--iconColor', iconColor);
+    });
+
+    return { activeTab, nextTab, previousTab, switchTab, tabs };
+  },
+};
+</script>
+
+<style scoped>
+.tabs {
+  display: flex;
+  justify-content: center; /* Center the tabs horizontally */
+  overflow-x: auto;
+  padding: 10px;
+  background: #ffffff;
+}
+
+.tabs-inner {
+  display: flex;
+  gap: 20px;
+  min-width: 100%;
+  justify-content: space-between; /* Ensure equal spacing between tabs */
+}
+
+.tab {
+  padding: 10px 20px;
+  font-size: 16px;
+  color: #333;
+  text-align: center;
+  flex-shrink: 0;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+}
+
+.tab::after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: var(--iconColor);
+  transform: scaleX(0);
+  transform-origin: left;
+  transition: transform 0.3s ease;
+}
+
+.tab.active {
+  color: var(--iconColor);
+}
+
+.tab.active::after {
+  transform: scaleX(1);
+}
+
+@media (max-width: 480px) {
   .tabs {
-    display: flex;
-    justify-content: center;
-    gap: 20px;
-    padding: 10px;
-    background: #ffffff;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    overflow-x: scroll;
   }
-  
+
+  .tabs-inner {
+    min-width: fit-content;
+    justify-content: flex-start;
+  }
+
   .tab {
-    padding: 10px 20px;
-    font-size: 16px;
-    color: #333;
-    text-align: center;
-    flex-grow: 1;
-    cursor: pointer;
-    transition: all 0.3s ease;
-    position: relative;
+    white-space: nowrap;
   }
-  
-  .tab::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 2px;
-    background-color: var(--iconColor);
-    transform: scaleX(0);
-    transform-origin: left;
-    transition: transform 0.3s ease;
-  }
-  
-  .tab.active {
-    color:var(--iconColor) ;
-  }
-  
-  .tab.active::after {
-    transform: scaleX(1);
-  }
-  </style>
-  
+}
+</style>

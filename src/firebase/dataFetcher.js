1ff48384bb6dde2c873a5c245f1689f39898112b
DeadlyBag <telmocarlos@gmx.net>
2023-07-21 10:33:15 +0200

    fixing

--- src/firebase/dataFetcher.js
+++ src/firebase/dataFetcher.js
@@ -27,35 +27,3 @@ export const fetchDataFromDatabase = async () => {
   
     return fakeData;
   }
-
-/*import { getDatabase, ref, onValue } from 'firebase/database';
-import { initializeApp } from 'firebase/app';
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  // ... your config here  apiKey: "AIzaSyDLeI2IiloFAkjAxOHjlYCkpCd9sE8Zynk",
-  authDomain: "social-97a77.firebaseapp.com",
-  projectId: "social-97a77",
-  storageBucket: "social-97a77.appspot.com",
-  messagingSenderId: "139606952850",
-  appId: "1:139606952850:web:13ba0dd4eaf14770abcf4b"
-}
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-export const fetchDataFromDatabase = async () => {
-  const topicsRef = ref(database, 'topics');
-
-  return new Promise((resolve, reject) => {
-    onValue(topicsRef, (snapshot) => {
-      const data = snapshot.val();
-      resolve(Object.values(data));
-    }, (error) => {
-      console.error('Error fetching data:', error);
-      reject(error);
-    });
-  });
-};
-*/
